8d54a8cddd4832fdb5f26497d47b432e8d44365c
stan smoltis <s.smoltis@gmail.com>
2019-05-10 23:37:02 +1000

    weather format string fix

--- openweathermap.py
+++ openweathermap.py
@@ -77,7 +77,7 @@ class OWM(object):
         ts = tzc.convert(datetime.utcfromtimestamp(response['dt']))
 
         weather = \
-            "{}, {}C @ {}%, cloudiness {}%, sunset {}, sunrise {}".format(
+            "{}, {}C @ {}%, cloudiness {}%, sunrise {}, sunset {}".format(
                 response['weather'][0]['description'],
                 response['main']['temp'],
                 response['main']['humidity'],
