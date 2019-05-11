94fff0d266a5d7d1e1ccfb39b399d9d385b9bb65
stan smoltis <s.smoltis@gmail.com>
2019-05-11 14:40:51 +1000

    hotfix missed weather cache refresh

--- weather_util.py
+++ weather_util.py
@@ -9,13 +9,20 @@ def get_weather():
     with shelve.open("weather.cache", writeback=True) as cached_weather:
         # find out if cache is stale
         present = datetime.now()
+        refresh = False
+        
         # every 3h refresh
         dt_key = list(cached_weather.keys())
         if dt_key:
             key = datetime.strptime(dt_key[0], '%Y-%m-%d %H:%M:%S')
             if timediff_min(key, present) <= 3*60:
                 weather = cached_weather[dt_key[0]]
+            else:
+                refresh = True
         else:
+            refresh = True
+
+        if refresh:
             #  if stale refresh cache
             obj = OWM()
             weather, dt = obj.get_weather()
