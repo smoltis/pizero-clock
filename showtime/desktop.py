791ef715f96a08ae038b0a9f229076571cbc0406
stan smoltis <s.smoltis@gmail.com>
2020-07-31 21:19:33 +1000

    added sensor reads and addressed flake8 lint issues

--- showtime/desktop.py
+++ showtime/desktop.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 # standard library
+import os
+import json
 from datetime import datetime
 
 #  my utility libraries
@@ -8,12 +10,35 @@ from .trains_util import get_trains
 from .weather_util import get_weather
 from .tides_util import get_tides
 
+
 def format_tides():
     tides = get_tides()
     m = 'Moon: {}%, {}'.format(tides['moon']['pct'], tides['moon']['phase'])
-    td = ["{} {}".format(item.get('desc'), item.get('ts')) for item in tides['tides']]
+    td = ["{} {}".format(item.get('desc'), item.get('ts'))
+          for item in tides['tides']]
     t = 'Tides at {}: {}'.format(tides['location'], ", ".join(td))
-    return "{} {}".format(m, t)
+    return "{}, {}".format(m, t)
+
+
+def read_sensor_data():
+    """
+    Read Temperature and Humidity Sensor data and display.
+    Prologue-TH wireless outdoor sensor is being used.
+    rtl_433 utility will read the data via radio data channel
+    and save the output to the file as a series of json objects.
+    The command is scheduled in Cron:
+    > rtl_433 -f 433920000 -R 03 -E quit -F json
+    """
+    filename = 'prologue/prologue.json'
+    degree_sign = u"\N{DEGREE SIGN}"
+    if os.path.exists(filename):
+        with open(filename) as f:
+            data = f.readlines()[-1]
+            if data:
+                th = json.loads(data)
+                return "Out T: {}{} @ {}%".format(th.get('temperature_C'),
+                                                  degree_sign,
+                                                  th.get('humidity'))
 
 
 def showtime():
@@ -28,3 +53,4 @@ def showtime():
         weather = get_weather()
         print('Weather: {}'.format(weather))
         print(format_tides())
+        print(read_sensor_data())
--- showtime/raspberrypi.py
+++ showtime/raspberrypi.py
@@ -2,6 +2,8 @@
 
 # standard library
 import time
+import os
+import json
 from datetime import datetime
 
 #  display drivers
@@ -30,9 +32,12 @@ def animation_dt(device, from_y, to_y, dt):
     while current_y != to_y:
         with canvas(device) as draw:
             text(draw, (0, 0), "l", fill="white", font=proportional(TINY_FONT))
-            text(draw, (1, current_y), hours_time, fill="white", font=proportional(CP437_FONT))
-            text(draw, (16, current_y), ":", fill="white", font=proportional(TINY_FONT))
-            text(draw, (18, current_y), min_time, fill="white", font=proportional(CP437_FONT))
+            text(draw, (1, current_y), hours_time, fill="white",
+                 font=proportional(CP437_FONT))
+            text(draw, (16, current_y), ":", fill="white",
+                 font=proportional(TINY_FONT))
+            text(draw, (18, current_y), min_time, fill="white",
+                 font=proportional(CP437_FONT))
         time.sleep(0.1)
         current_y += 1 if to_y > from_y else -1
 
@@ -41,9 +46,12 @@ def draw_time(device):
     hours = datetime.now().strftime('%H')
     minutes = datetime.now().strftime('%M')
     with canvas(device) as draw:
-        text(draw, (0, 1), hours, fill="white", font=proportional(CP437_FONT))
-        text(draw, (16, 1), ":", fill="white", font=proportional(TINY_FONT))
-        text(draw, (18, 1), minutes, fill="white", font=proportional(CP437_FONT))
+        text(draw, (0, 1), hours, fill="white",
+             font=proportional(CP437_FONT))
+        text(draw, (16, 1), ":", fill="white",
+             font=proportional(TINY_FONT))
+        text(draw, (18, 1), minutes, fill="white",
+             font=proportional(CP437_FONT))
 
 
 def minute_change(device):
@@ -53,9 +61,12 @@ def minute_change(device):
 
     def helper(current_y):
         with canvas(device) as draw:
-            text(draw, (0, 1), hours, fill="white", font=proportional(CP437_FONT))
-            text(draw, (15, 1), ":", fill="white", font=proportional(TINY_FONT))
-            text(draw, (17, current_y), minutes, fill="white", font=proportional(CP437_FONT))
+            text(draw, (0, 1), hours, fill="white",
+                 font=proportional(CP437_FONT))
+            text(draw, (15, 1), ":", fill="white",
+                 font=proportional(TINY_FONT))
+            text(draw, (17, current_y), minutes, fill="white",
+                 font=proportional(CP437_FONT))
             time.sleep(0.1)
 
     # for current_y in range(1, 9):
@@ -67,10 +78,32 @@ def minute_change(device):
 
 def format_tides():
     tides = get_tides()
-    m = 'Moon: {}%, {}'.format(tides['moon']['pct'], tides['moon']['phase'])
-    td = ["{} {}".format(item.get('desc'), item.get('ts')) for item in tides['tides']]
-    t = 'Tides at {}: {}'.format(tides['location'], ", ".join(td))
+    m = '>Moon: {}%, {}'.format(tides['moon']['pct'], tides['moon']['phase'])
+    td = ["{} {}".format(item.get('desc'), item.get('ts'))
+          for item in tides['tides']]
+    t = '>Tides at {}: {}'.format(tides['location'], ", ".join(td))
     return "{} {}".format(m, t)
+
+
+def read_sensor_data():
+    """
+    Read Temperature and Humidity Sensor data and display.
+    Prologue-TH wireless outdoor sensor is being used.
+    rtl_433 utility will read the data via radio data channel
+    and save the output to the file as a series of json objects.
+    The command is scheduled in Cron:
+    > rtl_433 -f 433920000 -R 03 -E quit -F json
+    """
+    filename = 'prologue/prologue.json'
+    degree_sign = u'\xb0'
+    if os.path.exists(filename):
+        with open(filename) as f:
+            data = f.readlines()[-1]
+            if data:
+                th = json.loads(data)
+                return ">Out T: {}{} @ {}%".format(th.get('temperature_C'),
+                                                   degree_sign,
+                                                   th.get('humidity'))
 
 
 def showtime():
@@ -86,10 +119,12 @@ def showtime():
         #  show next three trains
         train_times = [tm.strftime('%H:%M') for tm in get_trains()[:3]]
         trains = ', '.join(train_times)
-        show_message(device, 'Trains: ' + trains, fill="white",
+        show_message(device, '>Trains: ' + trains, fill="white",
                      font=proportional(CP437_FONT))
     weather = get_weather()
-    show_message(device, 'Weather: ' + weather, fill="white",
+    show_message(device, '>Weather: ' + weather, fill="white",
+                 font=proportional(CP437_FONT))
+    show_message(device, read_sensor_data(), fill="white",
                  font=proportional(CP437_FONT))
     show_message(device, format_tides(), fill="white",
                  font=proportional(CP437_FONT))
